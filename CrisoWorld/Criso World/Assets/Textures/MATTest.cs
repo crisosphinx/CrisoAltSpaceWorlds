69598f37503f3a747e6bdcb0445d7bd425fda942
Jeffrey Miller <Jeff3DAnimation@yahoo.com>
2021-11-07 18:45:43 -0500

    Progress update
    For what? I don't know.

---
+++ CrisoWorld/Criso World/Assets/Textures/MATTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEditor;
+
+public class MATTest : MonoBehaviour
+{
+    public Texture2D[] textures;
+    public Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Texture2D t in textures)
+        {
+            WaitForSeconds
+            material.SetTexture(t.name, t);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
